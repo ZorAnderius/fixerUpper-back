ba2659a60125522df720d4790a70ff05c7b491e3
ZorAnderius <gabriel.zorander@gmail.com>
2025-08-31 20:40:30 +0100

    add: cookie parser, rules for json and urlencoded requset body

--- src/serverConfig/initExpress.js
+++ src/serverConfig/initExpress.js
@@ -2,10 +2,17 @@ import express from "express";
 import cors from 'cors';
 import morgan from "./config/morganConfig.js";
 import corsOptions from "./config/corsConfig.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(morgan("tiny-colored"));
 app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json({
+  type: ['application/json'],
+}));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(express.static('public'));
 
 export default app;
