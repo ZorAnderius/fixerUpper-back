dbce7a4e92d27c947ed84a71bf7342fe0ece5bdb
ZorAnderius <gabriel.zorander@gmail.com>
2025-08-31 20:42:51 +0100

    add: helmet configuration

--- src/serverConfig/initExpress.js
+++ src/serverConfig/initExpress.js
@@ -1,13 +1,16 @@
 import express from "express";
 import cors from 'cors';
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
 import morgan from "./config/morganConfig.js";
 import corsOptions from "./config/corsConfig.js";
-import cookieParser from "cookie-parser";
+import helmetOption from "./config/helmetConfig.js";
 
 const app = express();
 
 app.use(morgan("tiny-colored"));
 app.use(cors(corsOptions));
+app.use(helmet(helmetOption));
 app.use(cookieParser());
 app.use(express.json({
   type: ['application/json'],
