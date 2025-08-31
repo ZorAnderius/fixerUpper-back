5020bbe7de8e21a6d7193aa3ba7dcd96364dda33
ZorAnderius <gabriel.zorander@gmail.com>
2025-08-31 20:24:59 +0100

    add: init express server

---
+++ index.js
@@ -0,0 +1,12 @@
+import setupServer from "./src/server.js"
+
+const bootstrap = () => {
+  try {
+    setupServer();
+  } catch (error) {
+    console.error('Error during server setup:', error);
+    process.exit(1);
+  }
+}
+
+bootstrap();
---
+++ src/server.js
@@ -0,0 +1,10 @@
+import app from "./serverConfig/initExpress.js";
+
+const PORT = 3000;
+
+const setupServer = () => {
+  app.listen(PORT, () => {
+    console.log(`\x1b[35mServer is running on the port ${PORT}\x1b[0m`);
+  })
+}
+export default setupServer;
