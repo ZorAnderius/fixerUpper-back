9117e5fd2522443ca990c8fb8144e1701d6a94b5
ZorAnderius <gabriel.zorander@gmail.com>
2025-09-01 20:38:09 +0100

    add: Cart sequelize model

---
+++ src/db/models/Cart.js
@@ -0,0 +1,23 @@
+import { DataTypes, Model } from "sequelize";
+
+class Cart extends Model {
+  static initModel(sequelize) {
+    return Cart.init({
+      id: { type: DataTypes.UUIDV4, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+      user_id: { type: DataTypes.UUIDV4, allowNull: false }
+    },
+      {
+        sequelize,
+        modelName: 'cart',
+        tableName: 'carts',
+        timestamps: false,
+        underscored: true
+      })
+  }
+
+  static associate(modl){
+
+  }
+}
+
+export default Cart;
