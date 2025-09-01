273d53ce97aa9a1c94c552df9780dd1f3c2b7b38
ZorAnderius <gabriel.zorander@gmail.com>
2025-09-01 21:15:01 +0100

    add: orderItem sequelize model

---
+++ src/db/models/OrderItem.js
@@ -0,0 +1,27 @@
+import { DataTypes, Model } from "sequelize";
+
+class OrderItem extends Model {
+  static initModel(sequelize) {
+    return OrderItem.init({
+      id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+      quantity: { type: DataTypes.INTEGER, allowNull: false, validate: { isInt: true, min: 0 } },
+      price: { type: DataTypes.DECIMAL(10, 2), allowNull: false, validate: { isDecimal: true, min: 0 } },
+      order_id: { type: DataTypes.UUID, allowNull: false },
+      product_id: { type: DataTypes.UUID, allowNull: false }
+    },
+      {
+        sequelize,
+        modelName: 'orderItem',
+        tableName: 'orderItems',
+        timestamps: false,
+        underscored: true
+      })
+  }
+
+  static associate(model) {
+
+  }
+
+}
+
+export default OrderItem;
