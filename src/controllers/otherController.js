57b823baa755d4abd27e5613a191462b66a8c55e
ZorAnderius <gabriel.zorander@gmail.com>
2025-09-03 11:27:14 +0100

    add: implement getAllProductStatus functionality with controller and service for retrieving product statuses

--- src/controllers/otherController.js
+++ src/controllers/otherController.js
@@ -1,11 +1,21 @@
 import responseMessage from "../constants/resMessage.js";
-import { getAllCategory } from "../services/otherServices.js";
+import { getAllCategory, getAllProductStatus } from "../services/otherServices.js";
 
-export const getAllCategoriesController = async (req, res) => {
+export const getAllCategoriesController = async (req, res, next) => {
   const data = await getAllCategory();
   res.json({
     status: 200,
     message: responseMessage.OTHER.CATEGORY_RETRIEVED,
+    data
+  });
+};
+
+
+export const getAllProductStatusController = async (req, res, next) => {
+  const data = await getAllProductStatus();
+  res.json({
+    status: 200,
+    message: responseMessage.OTHER.PRODUCT_STATUS_RETRIEVED,
     data
   });
 };
--- src/routes/otherRoutes.js
+++ src/routes/otherRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllCategoriesController } from "../controllers/otherController.js";
+import { getAllCategoriesController, getAllProductStatusController } from "../controllers/otherController.js";
 import { originGuards } from "../middlewares/middlewareSet.js";
 import secureInput from "../middlewares/secureInput.js";
 import apiLimit from "../middlewares/requestLimit/apiRequest.js";
@@ -8,5 +8,8 @@ import ctrlWrapper from "../utils/ctrlWrapper.js";
 const otherRoutes = express.Router();
 
 otherRoutes.get('/categories', [...originGuards, secureInput, ...apiLimit], ctrlWrapper(getAllCategoriesController));
+
+
+otherRoutes.get('/product-statuses', [...originGuards, secureInput, ...apiLimit], ctrlWrapper(getAllProductStatusController));
 
 export default otherRoutes;
