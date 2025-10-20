00874b3e0448f668c4832add4211cb27a294673a
ZorAnderius <gabriel.zorander@gmail.com>
2025-10-20 20:47:06 +0100

    Fix cookies: change SameSite to None for cross-domain support

--- src/utils/setCRSFTokenCookie.js
+++ src/utils/setCRSFTokenCookie.js
@@ -6,7 +6,7 @@ export const setCSRFTokenCookie = (res, token) => {
   const cookieOptions = {
     httpOnly: false,
     secure: env(ENV_VARS.NODE_ENV) === 'production',
-    sameSite: 'Strict',
+    sameSite: 'None',
     maxAge: MAX_AGE_CSRF_TOKEN * 1000,
   };
   res.cookie(CSRF_TOKEN_COOKIE, token, cookieOptions);
@@ -15,7 +15,7 @@ export const setCSRFTokenCookie = (res, token) => {
 export const clearCSRFTokenCookie = res => {
   res.clearCookie(CSRF_TOKEN_COOKIE, {
     httpOnly: false,
-    secure: env(ENV_VARIABLES.NODE_ENV) === 'production',
-    sameSite: 'Strict',
+    secure: env(ENV_VARS.NODE_ENV) === 'production',
+    sameSite: 'None',
   });
 };
